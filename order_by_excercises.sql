37e7d5ab7e111297abfda1d6e5a9f4849e4ba3f0
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-03-31 16:45:14 -0500

    corrected order_by_excercises

--- order_by_excercises.sql
+++ order_by_excercises.sql
@@ -19,7 +19,7 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 -- 5.
 SELECT * FROM employees WHERE last_name LIKE 'E%E'
     ORDER BY emp_no;
--- returns 899 rocords
+-- returns 899 records
 -- first row: 10021 Ramzi ERDE
 -- last row: 499648 Tadahiro ERDE
 
@@ -34,7 +34,7 @@ SELECT * FROM employees WHERE last_name LIKE 'E%E'
 SELECT * FROM employees
 	WHERE birth_date LIKE '____-12-25'
 		AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-	ORDER BY hire_date DESC;
+	ORDER BY birth_date, hire_date DESC;
 -- returns 362 records
 -- first row: Khun BERNINI
--- last row: Alselm CAPPELLO
+-- last row: Douadi PETTIS
